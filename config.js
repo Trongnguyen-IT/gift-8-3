4f64f1e69489b22bf9c31292430b26b296aaf70b
Trongnguyen <nguyennhattrong@gmail.com>
2025-03-07 19:33:33 +0700

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/';
+const eventName = 'Chúc Mừng Ngày 8-3';
+const titleCard = 'Tặng bé Mộng';
+const contentCard = 'Chúc cô giáo mầm non một ngày 8/3 thật hạnh phúc! tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em.';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
